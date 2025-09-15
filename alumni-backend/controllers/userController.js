d4be066f74ae298b5f10d2fbaebe61b3bd0ddef9
Pranav1632 <pranavgaikwad1632@gmail.com>
2025-09-15 22:28:54 +0530

    added alumini profile updatation process and tested succesfully in Postman

--- alumni-backend/controllers/userController.js
+++ alumni-backend/controllers/userController.js
@@ -1,14 +1,12 @@
 const User = require("../models/User");
 const StudentProfile = require("../models/StudentProfile");
-const AlumniProfile = require("../models/AlumniProfile"); // new import
+const AlumniProfile = require("../models/AlumniProfile");
 
-// GET logged-in user
+// GET logged in user + profile
 exports.getMe = async (req, res) => {
   try {
     const user = await User.findById(req.user.id).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    if (!user) return res.status(404).json({ message: "User not found" });
 
     let profile = null;
 
@@ -20,16 +18,17 @@ exports.getMe = async (req, res) => {
 
     res.json({ user, profile });
   } catch (err) {
+    console.error(err);
     res.status(500).json({ message: "Server Error" });
   }
 };
 
-// UPDATE profile
+// UPDATE logged in user profile
 exports.updateMe = async (req, res) => {
   try {
     const userId = req.user.id;
 
-    // Update basic user info
+    // Update basic User info
     const updatedUser = await User.findByIdAndUpdate(
       userId,
       { name: req.body.name, email: req.body.email },
