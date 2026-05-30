e2dcffef3955e146f4c331efb09c9d012603d629
Pranav1632 <pranavgaikwad1632@gmail.com>
2026-05-30 14:19:03 +0530

    ff

--- alumni-frontend/src/components/EventCalender.jsx
+++ alumni-frontend/src/components/EventCalender.jsx
@@ -8,7 +8,7 @@ export default function EventCalendar() {
 
   // Example events (later can fetch from backend)
   const events = [
-      { date: "2025-09-20", title: "🎉 Alumni Meetup" },
+      { date: "2025-09-20", title: "🎉 Alumnii Meetup" },
     { date: "2025-09-25", title: "🎤 Guest Lecture" },
     { date: "2025-09-05", title: "🏆 Hackathon" },
     { date: "2025-09-05", title: "Tech Fest" },
